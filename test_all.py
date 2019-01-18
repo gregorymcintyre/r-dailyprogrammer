a9c352ff9d8c2ed28696b9d1bef320cefda4497e
Fox <gregmcintyre1984@gmail.com>
2019-01-18 14:57:16 +1100

    made a repo for the tester removed from here

--- test_all.py
+++
@@ -1,9 +0,0 @@
-import os
-
-ListofFiles = os.listdir('.')
-
-for entry in ListofFiles:
-    if 'test' not in entry: 
-        if '.py' in entry:
-            print(entry)
-            os.system("python3 " + entry)
