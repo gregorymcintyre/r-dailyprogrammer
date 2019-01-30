f42c5a60b9b54c0075fd058128a2018595f3f031
Fox <gregmcintyre1984@gmail.com>
2019-01-30 11:11:48 +1100

    AdditivePersistance - with strings

---
+++ AdditivePersistance.py
@@ -0,0 +1,49 @@
+'''
+calculate the additive persistence of a number, defined as how many loops you have to do summing its digits until you get a single digit number. Take an integer N:
+
+    Add its digits
+
+    Repeat until the result has 1 digit
+
+The total number of iterations is the additive persistence of N.
+
+Your challenge today is to implement a function that calculates the additive persistence of a number.
+
+https://www.reddit.com/r/dailyprogrammer/comments/akv6z4/20190128_challenge_374_easy_additive_persistence/
+
+
+Bonus
+
+The really easy solution manipulates the input to convert the number to a string and iterate over it. Try it without making the number a strong, decomposing it into digits while keeping it a number.
+
+On some platforms and languages, if you try and find ever larger persistence values you'll quickly learn about your platform's big integer interfaces (e.g. 64 bit numbers)
+
+'''
+
+from test import test
+
+def AdditivePersistence(number):
+    cycles = 0
+    
+    while True:
+        value = 0
+        for digit in map(int, str(number)):
+            value += digit
+        if value > 9:
+            cycles += 1
+            number = value
+        else:
+            cycles += 1
+            return cycles
+
+print(AdditivePersistence(13))
+print(AdditivePersistence(1234))
+print(AdditivePersistence(9876))
+print(AdditivePersistence(199))
+
+#test(AdditivePersistence, 1, 13)
+#test(AdditivePersistence, 2, 1234)
+#test(AdditivePersistence, 2, 9876)
+#test(AdditivePersistence, 3, 199)
+
+
--- RouletteDoubles.py
+++ RouletteDoubles.py
@@ -54,6 +54,55 @@ def winAssessement(runs, winFloat, cash):
 
 #RouletteDouble(50, 100)
 
-print('Runs, Stop win float, starting cash')
-winAssessement(int(input()), float(input()), int(input()))
+#print('Runs, Stop win float, starting cash')
+#winAssessement(int(input()), float(input()), int(input()))
+#winAssessement(1000, 2, 100)
+
+
+def simpleTest(count):
+    winCount = 0
+    number=random.randint(0, 36)
+    for int in range(count):
+        number=random.randint(0, 36)
+        if number%2 == 0:
+            winCount+=1
+
+    return winCount/100
+
+print('simple test '+ str(simpleTest(100)))
+
+def wheelSim():
+    number = random.randint(0, 38)
+    if number == 37:
+        return '0'
+    elif number == 38:
+        return '00'
+    else:
+        return str(number)
+
+def betEdge2(amount):
+    if wheelSpin()%2 == 0:
+        return amount*2
+    else:
+        return 0
+
+def numberBet(amount):
+    if wheelSim == 19:
+        return amount*35
+    else:
+        return 0
+
+#cash = 0
+#for int in range(1000000):
+#    win = numberBet(2.5)
+#    if win > 0:
+#        cash += win
+#    else:
+#        cash -= 2.5
+#
+#print(cash)
+
+
+
+
 
