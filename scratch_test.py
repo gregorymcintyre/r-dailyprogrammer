daf11bd65eb04784b2706877395316dd3ff485f8
Fox <gregmcintyre1984@gmail.com>
2019-01-31 15:16:49 +1100

    rm scratch

--- scratch_test.py
+++ scratch_test.py
@@ -1,15 +1,6 @@
-def testing(*inp):
-    print(', '.join(map(str, inp[0::])))
-    #print(str(inp[-1]))
-    #str(v) for v in value_list
+from test import test
 
-def funct(inp, *out):
-    inp(', '.join(map(str, out)))
-    #print(', '.join(map(str, out)))
-    #inp(', '.join(map(str, out)))
+def scratchTest(int):
+    return int
 
-testing(1, 2)
-testing(1, 2, 3)
-
-funct(testing, 1)
-funct(testing, 1, 2, 3)
+test(scratchTest, 2, 'a')
